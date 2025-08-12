5c2ca85df2459e2b953cb3bde2da4a86c6ee96bd
Your Name <ahmed.m.sharara@gmail.com>
2025-08-12 10:24:23 +0400

    Remove unused pages and components: SupplementDetailPage, LoginPage, OnboardingPage, and SignupPage

--- src/pages/auth/OnboardingPage 2.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ConsolidatedOnboarding from '../../components/onboarding/ConsolidatedOnboarding'
-
-const OnboardingPage: React.FC = () => {
-  return <ConsolidatedOnboarding />
-}
-
-export default OnboardingPage
